de05f6f1f528242de36d56086478a8f239dd4699
Dima <soloveid87@mail.ru>
2022-11-17 16:55:12 +1000

    usecases and repository

---
+++ app/src/main/java/com/dima/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package com.dima.shoppinglist.domain
+
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun getShopList(): List<ShopItem> {
+        return shopListRepository.getShopList()
+    }
+}
