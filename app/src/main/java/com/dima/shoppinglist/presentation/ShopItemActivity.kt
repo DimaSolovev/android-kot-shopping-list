f48ad9b106dc6fc451a1d335d12ec3d30491f510
Dima <soloveid87@mail.ru>
2022-11-21 14:10:08 +1000

    ShopItemActivity

---
+++ app/src/main/java/com/dima/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.dima.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dima.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
